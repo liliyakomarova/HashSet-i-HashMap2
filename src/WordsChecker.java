802d7ed7726880a4d036203d9349e5e5f527b577
liliyakomarova <liliya.komarova.86@mail.ru>
2022-10-29 13:43:28 +0300

    Initial commit

---
+++ src/WordsChecker.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class WordsChecker {
+    protected String text;
+    protected Set<String> set = new HashSet<>();
+    public WordsChecker(String text) {
+        this.text = text;
+    }
+    public void textTransformation() {
+        String[] textArray = text.split(" ");
+        for (int i = 0; i < textArray.length; i++) {
+            if (textArray[i].contains(",") || textArray[i].contains(".")) {
+                set.add(textArray[i].substring(0, (textArray[i].length() - 1)).toLowerCase());
+            } else {
+                set.add(textArray[i].toLowerCase());
+            }
+        }
+    }
+    public boolean hasWord(String word) {
+        if (set.contains(word)) {
+            System.out.println("Слово " + word + " в тексте: ");
+            return true;
+        } else {
+            System.out.println("Слово " + word + " в тексте: ");
+            return false;
+        }
+    }
+}
